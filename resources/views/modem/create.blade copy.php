bfbf85714aa96e2409bef63aa0ce411759e3341b
Adit Sapari <adhitsafary@gmail.com>
2025-01-11 08:58:34 +0700

    baru

--- resources/views/modem/create.blade copy.php
+++ resources/views/modem/create.blade copy.php
@@ -31,6 +31,10 @@
                 <input type="text" class="form-control" id="id_mikrotik" name="id_mikrotik">
             </div>
             <div class="mb-3">
+                <label for="id_mikrotik" class="form-label">ID MikroTik</label>
+                <input type="text" class="form-control" id="id_mikrotik" name="id_mikrotik">
+            </div>
+            <div class="mb-3">
                 <label for="keterangan" class="form-label">Keterangan</label>
                 <input type="text" class="form-control" id="keterangan" name="keterangan">
             </div>
